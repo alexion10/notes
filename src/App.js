c024b4d1975031d50d51ee781ace421ae6544254
alexion10 <36454271+alexion10@users.noreply.github.com>
2022-11-04 11:38:47 +0200

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [item, setItem] = useState('');
+  const [notes, setNotes] = useState([]);
+  
+  const handleClick = () => {
+        if(item === '')
+          return;       
+        setNotes(prev => [...prev, {id: Math.round(Math.random() * 1000), note: item}])
+        setItem('');
+
+  }
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  }
+  const deleteFromList = (id) => {
+    console.log('sigo')
+    const list = notes.filter(item => item.id !== id)  
+    setNotes(list)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className='container'>
+      <div className='control-details'>
+        <div>
+          <label>Right your notes</label>
+          <input type="text" value={item} onChange={handleChange}/>
+        </div>
+        <button onClick={handleClick}>add</button>
+      </div>
+      <div className='notes-list'>{notes.map(i => <Note key={i.id} id={i.id} note={i.note} deleteFromList={deleteFromList}/>)}</div>
+    </div>
     </div>
   );
-}
 
+  }
+
+const Note = ({id, note, deleteFromList}) => {
+  const [mark, setMark] = useState(false);
+  const handleClick = () => {
+    deleteFromList(id);
+  }
+  return (
+    <div className='note' style={(mark) ? {background: "#5858859c"} : null}>
+      <input type="checkbox" checked={mark} onChange={()=>setMark(!mark)} />
+      <p>{note}</p>
+      <button onClick={handleClick}>delete</button>
+    </div>
+  )
+} 
 export default App;
